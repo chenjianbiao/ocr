6fa6c1518abc4db2f82b3dea4fb9335df9897129
chenjianbiao <hnxxpd@126.com>
2019-03-28 19:39:33 +0800

    officeroom

--- FindImageBBox.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import cv2
-import numpy as np
-
-class FindImageBBox(object):
-    def do(self):
-        print("test do")
--- PreprocessResizeKeepRatioFillBG.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-class PreprocessResizeKeepRatioFillBG(object):
-    def do(self,img):
-        print('test')
