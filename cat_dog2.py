0927fc12578281261ea2e341307355011f6bcd5a
chenjianbiao <hnxxpd@126.com>
2019-05-23 00:48:16 +0800

    cat_dog2

--- cat_dog2.py
+++ cat_dog2.py
@@ -24,30 +24,30 @@ os.mkdir(test_dogs_dir)
 fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
-dst = os.path.join(train_cats_dir, fname)
-shutil.copyfile(src, dst)
+    dst = os.path.join(train_cats_dir, fname)
+    shutil.copyfile(src, dst)
 fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
-dst = os.path.join(validation_cats_dir, fname)
-shutil.copyfile(src, dst)
+    dst = os.path.join(validation_cats_dir, fname)
+    shutil.copyfile(src, dst)
 fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
-dst = os.path.join(test_cats_dir, fname)
-shutil.copyfile(src, dst)
+    dst = os.path.join(test_cats_dir, fname)
+    shutil.copyfile(src, dst)
 fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
-dst = os.path.join(train_dogs_dir, fname)
-shutil.copyfile(src, dst)
+    dst = os.path.join(train_dogs_dir, fname)
+    shutil.copyfile(src, dst)
 fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
-dst = os.path.join(validation_dogs_dir, fname)
-shutil.copyfile(src, dst)
+    dst = os.path.join(validation_dogs_dir, fname)
+    shutil.copyfile(src, dst)
 fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
 for fname in fnames:
     src = os.path.join(original_dataset_dir, fname)
-dst = os.path.join(test_dogs_dir, fname)
-shutil.copyfile(src, dst)
+    dst = os.path.join(test_dogs_dir, fname)
+    shutil.copyfile(src, dst)
